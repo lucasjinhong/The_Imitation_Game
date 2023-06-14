b470635f1d65107b7f90d443e98c22ba735d59af
TaylorNTUT <t111368101@ntut.edu.tw>
2023-06-14 18:25:55 +0800

    [taylor] edit Level2.py

--- View/Story/Level2.py
+++ View/Story/Level2.py
@@ -29,12 +29,13 @@ class Level2:
 
         self.text += '在Binaropolis的中心，居民們正為如何對抗那個黑客而煩惱著。'
         self.text += '而就在此時，一道訊號忽然出現在一條被命名為 "Negation Alley" 的巷子中。'
-        self.text += f'這個訊號源自那位神秘的黑客，他留下了一串二進制碼：{answer_bin}。\n\n'
+        self.text += f'這個訊號源自那位神秘的黑客，他留下了一串二進制碼："{answer_bin}"。\n\n'
 
         path = 'Model/Story/Level2/Scene2.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'這串二進制碼{answer_bin}在Not英雄的能力作用下，會變成怎樣的結果？'
+        self.text += f'\n這串二進制碼 "{answer_bin}" 在Not英雄的能力作用下，會變成怎樣的結果？\n'
+        self.text += f'(請輸入8位數字)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
@@ -62,7 +63,7 @@ class Level2:
         }
 
         return self.parameters, self.text
-    
+
     def scene_3(self):
         answer_not = self.last_answer
         answer_and, answer_bin = Controller.codec('2', 'and_gate', answer_not)
@@ -73,14 +74,15 @@ class Level2:
         path = 'Model/Story/Level2/Scene3-1.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'這兩組訊號分別是 {answer_bin} 和經Not轉換過的 {answer_not}。'
+        self.text += f'\n這兩組訊號分別是 "{answer_bin}" 和經Not轉換過的 "{answer_not}" 。'
         self.text += '二者被黑客巧妙地連接在一起，每一個數字都對應著另一組數字的相應位置。'
         self.text += '這種關聯似乎並不隨機，而是有特定的規律。\n\n'
 
         path = 'Model/Story/Level2/Scene3-2.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'這兩串二進制碼({answer_bin}, {answer_not})在And的能力作用下，會變成怎樣的結果？'
+        self.text += f'\n這兩串二進制碼 "{answer_bin}" , "{answer_not}" 在And的能力作用下，會變成怎樣的結果？\n'
+        self.text += f'(請輸入8位數字)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
@@ -114,14 +116,14 @@ class Level2:
         answer_or, answer_bin = Controller.codec('2', 'or_gate', answer_and)
 
         self.text += '眾人屏息以待，And 終於開口：「我完成了運算。」'
-        self.text += f'他指著新的二進制碼{answer_and}說道：「這是我根據這兩串數字運算的結果。'
+        self.text += f'他指著新的二進制碼 {answer_and} 說道：「這是我根據這兩串數字運算的結果。'
         self.text += '我相信，這將帶給我們新的線索，也將使我們更接近真相。」\n\n'
 
         path = 'Model/Story/Level2/Scene4-1.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'\n在他們沿著 Union Avenue 深入的時候，一個新的訊號忽然閃爍著微弱的光芒出現在他們的眼前，那是一串二進制碼：{answer_bin}。'
-        self.text += '此時，他們看到黑客留下的另一個訊息：「在聯合中取得答案。」'
+        self.text += f'\n在他們沿著 Union Avenue 深入的時候，一個新的訊號忽然閃爍著微弱的光芒出現在他們的眼前，那是一串二進制碼： "{answer_bin}" 。'
+        self.text += '此時，他們看到黑客留下的另一個訊息：「在聯合中取得答案。」\n\n'
         self.text += '這似乎是個新的提示，也可能是另一個陷阱，'
         self.text += '無論如何，他們必須前進，他們必須去解開這個新的謎團。'
         self.text += '二者被黑客巧妙地連接在一起，每一個數字都對應著另一組數字的相應位置。'
@@ -130,7 +132,8 @@ class Level2:
         path = 'Model/Story/Level2/Scene4-2.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'\n如果將 And 處理過的二進制碼({answer_and})和新的訊號({answer_bin})進行Or的能力，會變成怎樣的結果？'
+        self.text += f'\n如果將 And 處理過的二進制碼 "{answer_and}" 和新的訊號 "{answer_bin}" 進行Or的能力，會變成怎樣的結果？\n'
+        self.text += f'(請輸入8位數字)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
@@ -158,7 +161,7 @@ class Level2:
         }
 
         return self.parameters, self.text
-    
+
     def scene_5(self):
         answer_or = self.last_answer
         answer_xor, answer_bin = Controller.codec('2', 'xor_gate', answer_or)
@@ -169,14 +172,15 @@ class Level2:
         path = 'Model/Story/Level2/Scene5-1.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'\n突然，他們的道路上出現了一個新的訊號，一串二進制碼：01100110。'
-        self.text += '這串碼與Or處理過的結果在空氣中相遇，形成了新的混淆碼，引起了他們的注意。'
+        self.text += f'\n突然，他們的道路上出現了一個新的訊號，一串二進制碼："{answer_bin}"。'
+        self.text += '這串碼與Or處理過的結果在空氣中相遇，形成了新的混淆碼，引起了他們的注意。\n\n'
         self.text += '同時，一段神秘的信息出現在他們的腦海中：「在分歧中尋找一致。」他們瞬間認出，這是黑客的新線索。\n\n'
 
         path = 'Model/Story/Level2/Scene5-2.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'\n請試著將 Or 處理過的二進制碼({answer_or})和新訊號({answer_bin})進行Xor的能力運算，會變成怎樣的結果？'
+        self.text += f'\n請試著將 Or 處理過的二進制碼 "{answer_or}" 和新訊號 "{answer_bin}" 進行Xor的能力運算，會變成怎樣的結果？\n'
+        self.text += f'(請輸入8位數字)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
@@ -211,9 +215,12 @@ class Level2:
         path = 'Model/Story/Level2/Scene6.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'這個混淆碼是一串16進制碼：{answer_hex}，旁邊還寫著一個訊息：「只有那些能理解我們的語言，才能找到我們。」'
-        self.text += '看來，他們需要先將這串混淆碼做一些轉換，才能進行運算。'
-        self.text += '請輸入這串16進制碼轉成2進制碼後的結果：'
+        self.text += f'這個混淆碼是一串16進制碼： "{answer_hex}" ，'
+        self.text += '旁邊還寫著一個訊息：「只有那些能理解我們的語言，才能找到我們。」\n\n'
+        self.text += '看來，他們需要先將這串混淆碼做一些轉換，才能進行運算。\n'
+        self.text += '請輸入這串16進制碼轉成2進制碼後的結果：\n'
+        self.text += '(以8位數字為一組，每一組之間空一個半形空格，\n'
+        self.text += ' Ex：00000000 11111111 00000000 11111111)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
@@ -231,7 +238,7 @@ class Level2:
                 'correct': '(答案正確)',
                 'wrong': '(答案錯誤)',
                 'answer': answer_bin,
-                'hint': '提示：一個16進制碼為4個2進制碼(A: 10, B:11, C: 12, D: 13, E: 14, F: 15)',
+                'hint': '提示：一位16進制碼為4位2進制碼(A: 10, B:11, C: 12, D: 13, E: 14, F: 15)',
                 'solution': f'正確答案是：{answer_bin}'
             },
             'config': {
@@ -241,7 +248,7 @@ class Level2:
         }
 
         return self.parameters, self.text
-    
+
     def scene_7(self):
         answer_bin = self.last_answer.split(' ')
         answer_all = Controller.codec('2', 'all_in_one', answer_bin)
@@ -252,12 +259,14 @@ class Level2:
         path = 'Model/Story/Level2/Scene7.txt'
         self.text += Controller.tools(path)
 
-        self.text += '現在，請根據之前的訊息，填入下面這四個空格：\n'
+        self.text += '現在，請根據之前的訊息，填入下面這四個空格：\n\n'
         self.text += f'1. 最一開始出現的是 {answer_bin[0]} ，經過Not能力運算後，得到 `A`\n'
         self.text += f'2. 把 `A` 和 {answer_bin[1]} 進行And能力運算後，得到 `B`\n'
         self.text += f'3. 把 `B` 和 {answer_bin[2]} 進行Or能力運算後，得到 `C`\n'
         self.text += f'4. 把 `C` 和 {answer_bin[3]} 進行Xor能力運算後，得到 `D`\n\n'
-        self.text += '請照順序輸入A, B, C, D(以八個數字為一組，每一組之間空一個半形空格，Ex：00000000 11111111 00000000 11111111)：'
+        self.text += '請照順序輸入A, B, C, D：\n'
+        self.text += '(以8位數字為一組，每一組之間空一個半形空格，\n'
+        self.text += ' Ex：00000000 11111111 00000000 11111111)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
@@ -285,14 +294,14 @@ class Level2:
         }
 
         return self.parameters, self.text
-    
+
     def scene_8(self):
         path = 'Model/Story/Level2/Scene8-1.txt'
         self.text += Controller.tools(path)
 
-        self.text += '四位英雄對視一眼，他們知道，這一次的挑戰將比之前的還要艱鉅。'
-        self.text += '這不僅僅是一場對他們理解力和運算能力的考驗，更是對他們堅韌意志的挑戰。'
-        self.text += f'他們再次集中全力，將先前從16進制轉換為的2進制碼 {self.last_question} 以及解答 {self.last_answer} '
+        self.text += '\n四位英雄對視一眼，他們知道，這一次的挑戰將比之前的還要艱鉅。'
+        self.text += '這不僅僅是一場對他們理解力和運算能力的考驗，更是對他們堅韌意志的挑戰。\n\n'
+        self.text += f'他們再次集中全力，將先前從16進制轉換為的2進制碼 {self.last_question} 以及解答 {self.last_answer} ──── A, B, C, D '
         self.text += '進行Not, And, Or, Xor的運算，然而，嘗試了幾次之後，很快就發現事情沒有這麼簡單。\n\n'
 
         path = 'Model/Story/Level2/Scene8-2.txt'
@@ -330,14 +339,18 @@ class Level2:
         last_answer = self.last_answer.split(' ')
         last_answer[3] = '________'
         self.last_answer = ' '.join(last_answer)
-
         answer_all = Controller.codec('2', 'last_codec', last_answer, last_question)
 
         path = 'Model/Story/Level2/Scene9.txt'
         self.text += Controller.tools(path)
 
-        self.text += f'請輸入這兩段數字({self.last_question}和{self.last_answer}) 經過Xor And Or Not運算後的結果：\n'
-        self.text += '(以八個數字為一組，每一組之間空一個半形空格，Ex：00000000 11111111 00000000 11111111)'
+        self.text += '\n請輸入這兩段數字，\n'
+        self.text += f' "{self.last_question}" 和\n'
+        self.text += f' "{self.last_answer}────A, B, C, D，(D這組數字，因為Not英雄發現可能有誤，而被屏蔽)\n'
+        self.text += '經過Xor And Or Not運算後的結果：\n'
+
+        self.text += '(以8位數字為一組，每一組之間空一個半形空格，\n'
+        self.text += ' Ex：00000000 11111111 00000000 11111111)'
 
         self.parameters['config']['button_enter'] = True
         self.parameters['config']['button_conti'] = False
